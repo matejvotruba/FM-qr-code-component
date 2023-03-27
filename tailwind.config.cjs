6413a47c830f55ef46c72ec01f4c2a78feae42ff
matejvotruba <ma.votruba@gmail.com>
2023-03-27 20:21:53 +0200

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'light-gray': 'hsl(212, 45%, 89%)',
+        'grayish-blue': 'hsl(220, 15%, 55%)',
+        'dark-blue': 'hsl(218, 44%, 22%)'
+      },
+      fontFamily: {
+        'body': ['Outfit', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
